b3ffbc61104afa17b491fba932cb0239dd7b0d04
Miguel Ángel Aguilar <miaguicam@gmail.com>
2018-07-14 17:57:20 +0200

    added lombok dependency

---
+++ app/src/main/java/model/Character.java
@@ -0,0 +1,18 @@
+package model;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+@Data
+@Getter
+@Setter
+@Accessors(fluent = true)
+public class Character {
+
+    private String name;
+    private String photo;
+    private String cover;
+    private String description;
+}
