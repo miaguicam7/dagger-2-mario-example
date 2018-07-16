9dafe50ef964e168276d967ab7f48a93017f3f0f
Miguel Ángel Aguilar <miaguicam@gmail.com>
2018-07-16 23:17:34 +0200

    refactoricing dagger2

--- app/src/main/java/di/CharacterComponent.java
+++ app/src/main/java/di/CharacterComponent.java
@@ -4,11 +4,13 @@ package di;
 import javax.inject.Singleton;
 
 import dagger.Component;
+import view.activity.CharacterActivity;
 
 @Singleton
 @Component(modules = CharacterModule.class)
 public interface CharacterComponent {
 //TODO: queda indicar donde se va a inyectar el componente
+void inject(CharacterActivity characterActivity);
 }
 
 
--- app/src/main/java/view/activity/CharacterActivity.java
+++ app/src/main/java/view/activity/CharacterActivity.java
@@ -1,21 +1,42 @@
 package view.activity;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ProgressBar;
 
 import com.example.miaguicam.mario_kart_dagger2.R;
 
 import java.util.List;
 
+import javax.inject.Inject;
+
+import butterknife.BindView;
 import view.presenter.CharacterPresenter;
 import view.presenter.Presenter;
 
 public class CharacterActivity extends BaseActivity implements CharacterPresenter.View{
 
+    private final static int ELEVATION_DP = 2;
+
+    @BindView(R.id.view_pager)
+    ViewPager pager;
+    @BindView(R.id.progress_detail)
+    ProgressBar detailProgress;
+
+    @Inject
+    CharacterPresenter presenter;
+
     @Override
     protected int getLayoutResID() {
-        return 0;
+
+        return R.layout.activity_characters;
     }
+
+ /*   private void initializeDagger() {
+        SuperMarioKartApplication app = (SuperMarioKartApplication) getApplication();
+        app.getCharactersComponent().inject(this);
+    }*/
 
     @Override
     public void onPointerCaptureChanged(boolean hasCapture) {
