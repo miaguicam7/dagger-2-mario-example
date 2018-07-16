c5d2dda70ac6e60258950100957e0d0d76151fa4
unknown <maaguilar@profile.es>
2018-07-16 15:02:04 +0200

    added list of characters

--- app/src/main/java/data/FakeDataSource.java
+++ app/src/main/java/data/FakeDataSource.java
@@ -20,7 +20,14 @@ public class FakeDataSource implements DataSource {
     @Override
     public List<Character> provideCharacterList() {
         addCharacter(getMarioBros());
-        
+        addCharacter(getLuigi());
+        addCharacter(getDaisy());
+        addCharacter(getToad());
+        addCharacter(getYoshi());
+        addCharacter(getDkong());
+        addCharacter(getWario());
+        addCharacter(getBowser());
+
         return this.characters;
     }
 
@@ -57,6 +64,246 @@ public class FakeDataSource implements DataSource {
                 .abilities(abilities)
                 .build();
     }
+
+
+    private Character getLuigi() {
+
+        final int accelerate = 97;
+        final int steer = 95;
+        final int brake = 73;
+        final int reverse = 97;
+        final int lookBehind = 87;
+        final int drift = 88;
+
+        final String id = "2";
+        final String name = "Luigi";
+        final int photo = R.drawable.ic_luigi;
+        final int cover = R.mipmap.luigi;
+        final String description =
+                "Similar to his brother, Mario, his kart also have average status across the board. Also like Mario, when computer-controlled, his main item is the Star.";
+        final Abilities abilities = Abilities.builder()
+                .accelerate(String.valueOf(accelerate))
+                .brake(String.valueOf(steer))
+                .drift(String.valueOf(brake))
+                .lookBehind(String.valueOf(reverse))
+                .reverse(String.valueOf(lookBehind))
+                .steer(String.valueOf(drift))
+                .build();
+
+        return Character.builder()
+                .id(String.valueOf(id))
+                .name(name)
+                .photo(photo)
+                .cover(cover)
+                .description(description)
+                .abilities(abilities)
+                .build();
+    }
+
+    private Character getDaisy() {
+
+        final int accelerate = 88;
+        final int steer = 95;
+        final int brake = 85;
+        final int reverse = 97;
+        final int lookBehind = 89;
+        final int drift = 82;
+
+        final String id = "3";
+        final String name = "Daisy";
+        final int photo = R.drawable.ic_daisy;
+        final int cover = R.mipmap.daisy;
+        final String description =
+                "Princess Daisy is a “tomboy.” I’m not sure how this boyishness manifests itself in the character design—her choice of an unflattering yellow dress, maybe? But this “tomboy” stuff is euphemistic nonsense, anyway. I imagine Daisy is a closeted lesbian, plucked from the socially liberal dream world of Super Mario Land’s Sarasaland and forced to mold her identity to the Mushroom Kingdom’s rigid gender roles.";
+        final Abilities abilities = Abilities.builder()
+                .accelerate(String.valueOf(accelerate))
+                .brake(String.valueOf(steer))
+                .drift(String.valueOf(brake))
+                .lookBehind(String.valueOf(reverse))
+                .reverse(String.valueOf(lookBehind))
+                .steer(String.valueOf(drift))
+                .build();
+
+        return Character.builder()
+                .id(String.valueOf(id))
+                .name(name)
+                .photo(photo)
+                .cover(cover)
+                .description(description)
+                .abilities(abilities)
+                .build();
+    }
+
+    private Character getToad() {
+
+        final int accelerate = 79;
+        final int steer = 98;
+        final int brake = 86;
+        final int reverse = 89;
+        final int lookBehind = 90;
+        final int drift = 93;
+
+        final String id = "4";
+        final String name = "Toad";
+        final int photo = R.drawable.ic_toad;
+        final int cover = R.mipmap.toad;
+        final String description =
+                "Below par speed-wise. His kart turns the best, but often loses control when hit by larger opponents. Like Princess Peach, he'll try throwing mushrooms in your way when he's controlled by the computer.";
+        final Abilities abilities = Abilities.builder()
+                .accelerate(String.valueOf(accelerate))
+                .brake(String.valueOf(steer))
+                .drift(String.valueOf(brake))
+                .lookBehind(String.valueOf(reverse))
+                .reverse(String.valueOf(lookBehind))
+                .steer(String.valueOf(drift))
+                .build();
+
+        return Character.builder()
+                .id(String.valueOf(id))
+                .name(name)
+                .photo(photo)
+                .cover(cover)
+                .description(description)
+                .abilities(abilities)
+                .build();
+    }
+
+    private Character getYoshi() {
+
+        final int accelerate = 89;
+        final int steer = 98;
+        final int brake = 86;
+        final int reverse = 82;
+        final int lookBehind = 96;
+        final int drift = 88;
+
+        final String id = "5";
+        final String name = "Yoshi";
+        final int photo = R.drawable.ic_yoshi;
+        final int cover = R.mipmap.yoshi;
+        final String description =
+                "Yoshi is the green dinosaur-like creature from Super Mario World. His kart has the highest acceleration. When playing against him, he'll try throwing eggs in your way.";
+        final Abilities abilities = Abilities.builder()
+                .accelerate(String.valueOf(accelerate))
+                .brake(String.valueOf(steer))
+                .drift(String.valueOf(brake))
+                .lookBehind(String.valueOf(reverse))
+                .reverse(String.valueOf(lookBehind))
+                .steer(String.valueOf(drift))
+                .build();
+
+        return Character.builder()
+                .id(String.valueOf(id))
+                .name(name)
+                .photo(photo)
+                .cover(cover)
+                .description(description)
+                .abilities(abilities)
+                .build();
+    }
+
+    private Character getDkong() {
+
+        final int accelerate = 90;
+        final int steer = 98;
+        final int brake = 89;
+        final int reverse = 97;
+        final int lookBehind = 89;
+        final int drift = 96;
+
+        final String id = "6";
+        final String name = "Donkey Kong";
+        final int photo = R.drawable.ic_dkong;
+        final int cover = R.mipmap.dk;
+        final String description =
+                "The son of the famous gorilla, Donkey Kong. His kart's attributes are similar to Bowser's. His main item is bananas, which is what the computer uses when controlling him.";
+        final Abilities abilities = Abilities.builder()
+                .accelerate(String.valueOf(accelerate))
+                .brake(String.valueOf(steer))
+                .drift(String.valueOf(brake))
+                .lookBehind(String.valueOf(reverse))
+                .reverse(String.valueOf(lookBehind))
+                .steer(String.valueOf(drift))
+                .build();
+
+        return Character.builder()
+                .id(String.valueOf(id))
+                .name(name)
+                .photo(photo)
+                .cover(cover)
+                .description(description)
+                .abilities(abilities)
+                .build();
+    }
+
+    private Character getWario() {
+
+        final int accelerate = 98;
+        final int steer = 92;
+        final int brake = 86;
+        final int reverse = 88;
+        final int lookBehind = 96;
+        final int drift = 89;
+
+        final String id = "7";
+        final String name = "Wario";
+        final int photo = R.drawable.ic_wario;
+        final int cover = R.mipmap.wario;
+        final String description =
+                "Wario's dirty tricks bring him dangerously close to disqualification but somehow is always greenlit for the seasons that follow. Wario is the torrential counterpart and brother to Waluigi.";
+        final Abilities abilities = Abilities.builder()
+                .accelerate(String.valueOf(accelerate))
+                .brake(String.valueOf(steer))
+                .drift(String.valueOf(brake))
+                .lookBehind(String.valueOf(reverse))
+                .reverse(String.valueOf(lookBehind))
+                .steer(String.valueOf(drift))
+                .build();
+
+        return Character.builder()
+                .id(String.valueOf(id))
+                .name(name)
+                .photo(photo)
+                .cover(cover)
+                .description(description)
+                .abilities(abilities)
+                .build();
+    }
+
+    private Character getBowser() {
+
+        final int accelerate = 89;
+        final int steer = 94;
+        final int brake = 87;
+        final int reverse = 82;
+        final int lookBehind = 94;
+        final int drift = 87;
+
+        final String id = "8";
+        final String name = "Bowser";
+        final int photo = R.drawable.ic_bowser;
+        final int cover = R.mipmap.bowser;
+        final String description =
+                "Bowser is the king of the turtle-like creatures called Koopas. His kart is good at crashing into other karts, due to the size. He'll usually try throwing lava balls to stop you.";
+        final Abilities abilities = Abilities.builder()
+                .accelerate(String.valueOf(accelerate))
+                .brake(String.valueOf(steer))
+                .drift(String.valueOf(brake))
+                .lookBehind(String.valueOf(reverse))
+                .reverse(String.valueOf(lookBehind))
+                .steer(String.valueOf(drift))
+                .build();
+
+        return Character.builder()
+                .id(String.valueOf(id))
+                .name(name)
+                .photo(photo)
+                .cover(cover)
+                .description(description)
+                .abilities(abilities)
+                .build();
+    }
+
 
     private void addCharacter(Character character) {
         this.characters.add(character);
