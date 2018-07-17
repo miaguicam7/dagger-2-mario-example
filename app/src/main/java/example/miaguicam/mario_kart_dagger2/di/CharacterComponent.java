146c8396648e362a86fa46df74f2347f2e55a199
unknown <maaguilar@profile.es>
2018-07-17 09:52:00 +0200

    done: -> added roboelectric and mockito dependencies -> reorderer test folder items

--- app/src/main/java/example/miaguicam/mario_kart_dagger2/di/CharacterComponent.java
+++ app/src/main/java/example/miaguicam/mario_kart_dagger2/di/CharacterComponent.java
@@ -9,8 +9,7 @@ import example.miaguicam.mario_kart_dagger2.view.activity.CharacterActivity;
 @Singleton
 @Component(modules = CharacterModule.class)
 public interface CharacterComponent {
-//TODO: queda indicar donde se va a inyectar el componente
-void inject(CharacterActivity characterActivity);
+    void inject(CharacterActivity characterActivity);
 }
 
 
